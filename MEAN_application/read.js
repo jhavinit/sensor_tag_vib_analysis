4ebc9631160ff88643c838b1be9364b85f6c0183
jhavinit <vj76223@gmail.com>
2019-10-04 18:25:08 +0530

    Graph thing started

--- MEAN_application/read.js
+++ MEAN_application/read.js
@@ -10,6 +10,20 @@ var docClient = new AWS.DynamoDB.DocumentClient();
 var table = "DhjFNng38H3AQZ7p_sensor_tag_data";
 var uid_from_file,x = 1,locked = 1,prev_req = -1;
 
+function write_into_live_file(filename,insert_data){
+  fs.writeFile(filename, insert_data, function (err) {
+    if (err) throw err;
+    //console.log('Saved!');
+  });  
+  }
+  
+  function write_into_historical_file(filename,insert_data){
+  fs.appendFile(filename,insert_data, function (err) {
+    if (err) throw err;
+    //console.log('Updated!');
+  });
+  }
+
 function update_uid_file(uiid){
 fs.writeFile('read_from_dynamodb.txt', (uiid + 1), function (err) {
   if (err) throw err; 
@@ -43,6 +57,27 @@ docClient.get(params, function(err, data) {
         console.log("UID = ",uid_from_file);
         console.log("GetItem succeeded:", JSON.stringify(data, null, 2));
         if(!(JSON.stringify(data, null, 2).length == 2)){
+          //console.log(data["Item"]["x_axis"]);
+            x_coordinate = data["Item"]["x_axis"];
+            x_coordinate_live_data = '[[' + (new Date()).getTime() + ',' + x_coordinate + ']]';
+            write_into_live_file('../x_live_data.json',x_coordinate_live_data);
+            x_coordinate_historical_data = ',' + '[' + (new Date()).getTime() + ',' + x_coordinate + ']';
+            write_into_historical_file('../x_historical_data.txt', x_coordinate_historical_data);
+          
+          //console.log(data["Item"]["y_axis"]);
+          y_coordinate = data["Item"]["y_axis"];
+          y_coordinate_live_data = '[[' + (new Date()).getTime() + ',' + y_coordinate + ']]';
+          write_into_live_file('../y_live_data.json',y_coordinate_live_data);
+          y_coordinate_historical_data = ',' + '[' + (new Date()).getTime() + ',' + y_coordinate + ']';
+          write_into_historical_file('../y_historical_data.txt', x_coordinate_historical_data);
+        
+          //console.log(data["Item"]["z_axis"]);
+          z_coordinate = data["Item"]["z_axis"];
+          z_coordinate_live_data = '[[' + (new Date()).getTime() + ',' + z_coordinate + ']]';
+          write_into_live_file('../z_live_data.json',z_coordinate_live_data);
+          z_coordinate_historical_data = ',' + '[' + (new Date()).getTime() + ',' + z_coordinate + ']';
+          write_into_historical_file('../z_historical_data.txt', z_coordinate_historical_data);
+        
   x = uid_from_file; 
   update_uid_file(uid_from_file);
   locked = 1;
